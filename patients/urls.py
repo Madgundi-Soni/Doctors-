a2fee2aca41e31645affd47a3e9a9172ca59f1f7
Soni Madgundi <88282648+Madgundi-Soni@users.noreply.github.com>
2024-08-04 10:41:43 +0530

    Add files via upload

--- patients/urls.py
+++ patients/urls.py
@@ -5,5 +5,6 @@ from django.contrib.auth import authenticate, login,logout
 urlpatterns = [
     path('dashboard/<int:id>/',views.Patient_dashboard,name='patient_dashboard'),   
     path('logout_view/',views.logout_view,name='logout_view'),   
+    path('blogs_list/',views.Blogs_list,name='blogs_list'),   
 
 ]
--- patients/views.py
+++ patients/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render,redirect
 from django.http import HttpResponse
 from user_details.models import User, Address
 from django.contrib.auth import authenticate, login,logout
+from doctors.models import Blogs,Categories
+
 
 def Patient_dashboard(request,id):
     context={}
@@ -16,3 +18,14 @@ def logout_view(request):
     logout(request)
     return redirect('login_page')
 
+
+def Blogs_list(request):
+    context={}
+    if request.GET.get('cat'):
+        cat = request.GET.get('cat')
+        context['blogs']=Blogs.objects.filter(is_draft=False,categories_id=cat).order_by('-id')    
+    else:
+        context['blogs']=Blogs.objects.filter(is_draft=False).order_by('-id')    
+    context['cat']=Categories.objects.filter()
+    
+    return render(request,'patient/patient_blogs.html',context)
